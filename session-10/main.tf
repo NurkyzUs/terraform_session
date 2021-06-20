5e538e2091f6dc0ea0f93d423d619434fe6fe099
nurkyzus <nurkyz.usv@gmail.com>
2021-06-20 20:29:52 +0000

    s3.tf modified

---
+++ session-10/main.tf
@@ -0,0 +1,15 @@
+module "s3_module" {
+  source = "github.com/NurkyzUs/terraform_may/modules/s3"
+  env    = "dev"
+}
+
+// https://github.com/NurkyzUs/terraform_may/tree/main/modules/s3
+
+// https            = protocol
+// github.com       = source code management
+// nurkyzus         = organization
+// terraform_may    = repository
+// tree             = branches
+// main             = branch 
+// modules          = sub-directory
+// s3               = sub-directory
